b7f5894800978eada33468e7eb0f2740c60a8756
Tolkun Asanaly <tolkusha@gmail.com>
2020-01-05 12:22:25 -0500

    repl practice

--- src/repl/Citizens.java
+++ src/repl/Citizens.java
@@ -4,17 +4,30 @@ public class Citizens {
     public static void main(String[] args) {
 
 //      -Declare int variables: seniorCitizens, nonSeniorCitizens, age
-        int seniorCitizens, nonSeniorCitizens, age;
+        int seniorCitizens, nonSeniorCitizens;
 //-Create a Scanner object
         Scanner scan = new Scanner(System.in);
 
 //- Ask user to enter count for seniorCitizens and nonSeniorCitizens using message:
 //"Enter current count for seniorCitizens and nonSeniorCitizens:"
         System.out.println("Enter current count for seniorCitizens and nonSeniorCitizens:");
+        seniorCitizens = scan.nextInt();
+        nonSeniorCitizens=scan.nextInt();
 
 //
 //-Ask user to enter age:
 //"What is new citizen's age?"
+        System.out.println("What is new citizen's age?");
+        int age= scan.nextInt();
+        if (age >= 65){
+            seniorCitizens++;
+            System.out.println("Senior Citizen");
+        }else if(age<65){
+            nonSeniorCitizens++;
+            System.out.println("Non-Senior Citizen");
+        }
+        System.out.println("New seniorCitizens count ValueOfVariable " + seniorCitizens);
+        System.out.println("New nonSeniorCitizens count ValueOfVariable " + nonSeniorCitizens);
 //
 //-if citizen's age is more than or equals to 65 then print "Senior Citizen" and increment seniorCitizens variable by 1.
 //
